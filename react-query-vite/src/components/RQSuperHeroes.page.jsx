b9a2ba8df3249c7ed0b450e6617693090c49e47e
WinWinMaw4 <mawinwinmaw4@gmail.com>
2023-11-06 00:02:37 +0630

    fetch data with useQuery

--- react-query-vite/src/components/RQSuperHeroes.page.jsx
+++ react-query-vite/src/components/RQSuperHeroes.page.jsx
@@ -1,12 +1,46 @@
+import { useQuery } from "react-query"
 import NavBar from "./layout/NavBar"
+import axios from "axios"
+
+const fetchSuperHeroesData = () =>{
+  return axios.get('http://localhost:4000/superheroes')
+}
 
 const RQSuperHeroes = () => {
+const {isLoading, data} = useQuery('super-heroes',fetchSuperHeroesData)
+
+if(isLoading) return <h2>Loading...</h2>
   return (
     <>
     <NavBar />
-    RQSuperHeroes.page
+    <section>
+      <h2 className="text-xl font-bold">RQ Super Heroes</h2>
+      {data?.data.map((hero)=>{
+        return <div key={hero.id}>{hero.name}</div>
+      })}
+    </section>
     </>
   )
 }
+
+
+// function fetchData(){
+//   const {isLoading, error, data} = useQuery('superHeroesData',()=> 
+//     fetch('http://localhost:4000/superheroes').then(res=>
+//     res.json())
+//   )
+//   if (isLoading) return 'Loading...'
+
+//   if (error) return 'An error has occurred: ' + error.message
+//   return (
+//     <div>
+//       <h1>{data.name}</h1>
+//       <p>{data.description}</p>
+//       <strong>👀 {data.subscribers_count}</strong>{' '}
+//       <strong>✨ {data.stargazers_count}</strong>{' '}
+//       <strong>🍴 {data.forks_count}</strong>
+//     </div>
+//   )
+// }
 
 export default RQSuperHeroes
