31c6bbabd066896540edeb18351070de17a16a1b
WinWinMaw4 <mawinwinmaw4@gmail.com>
2023-11-05 22:30:21 +0630

    data fetching with useEffect and useState

--- react-query-vite/src/components/SupreHeroes.page.jsx
+++ react-query-vite/src/components/SupreHeroes.page.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react"
 import NavBar from "./layout/NavBar"
+import axios from "axios";
 
 const SupreHeroes= () => {
+  const [isLoading,setIsLoading] = useState(true);
+  const [data,setData] = useState("");
+
+  useEffect(()=>{
+    axios.get('http://localhost:4000/superheroes').then((res)=>{
+      setData(res.data)
+      setIsLoading(false)
+    })
+  },[])
+
+  if(isLoading){
+    return <h2>Loading....</h2>
+  }
   return (
     <>
     <NavBar />
-    uperHeroes.page
+    <div>
+      <h2 className="text-xl font-bold">Super Heroes Page</h2>
+      {data.map((hero)=>{
+        return <div key={hero.id}>{hero.name}</div>
+      })}
+    </div>
     </>
   )
 }
